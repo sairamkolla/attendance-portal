98e4540bce3ad6804ba82e98ef0cffe23547fbae
sairam kolla <sai.kolla24@gmail.com>
2015-11-15 02:27:27 +0530

    dependencies added and data-app created

--- attendence/attendence/settings.py
+++ attendence/attendence/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'portaldata',
+    'rest_framework',
+    'corsheaders',
 )
 
 MIDDLEWARE_CLASSES = (
